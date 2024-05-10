f629acf3c3ea3da621e7b14096dc16b0e4e08e2a
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-05-10 19:31:50 +0530

    Frontend added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+
+import { StateContextProvider } from "./context";
+import { ErrorComponent } from "./components";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorComponent message={'Sorry some error Occured !!!'}/>,
+    children: [
+      { index: true, element: <Home/> },
+      { path: "profile", element: <Profile/> },
+      { path: "create-campaign", element: <CreateCampaign /> },
+      { path: "campaign-details/:id", element: <CampaignDetails /> },
+    ],
+  },
+]);
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "sepolia";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -17,7 +37,9 @@ root.render(
       clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
     >
-      <App />
+      <StateContextProvider>
+        <RouterProvider router={router} />
+      </StateContextProvider>
     </ThirdwebProvider>
   </React.StrictMode>
 );
